3c6809c518cc423134778541612f0b78e0922c60
Vinda29 <vindahandayani29@students.amikom.ac.id>
2023-04-04 23:39:48 +0700

    Add project files.

---
+++ Vinda/Latihan Taxi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Taxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Latihan_Taxi taxi = new Latihan_Taxi();
+            taxi.Drivername = "Vinda";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropPffPassenger();
+
+            Console.WriteLine();
+        }
+    }
+}
